75f01daa2981edbbc82034846e4e30874f47724b
Konstantin <kostya.demchenko@gmail.com>
2023-11-21 20:28:05 +0200

    opencl matrix multiply 3 ways: naive, submatrix, submatrix+WPT

--- OpenCL-matrix-mult-cached/OpenCL-matrix-mult-cached/OpenCL-matrix-mult-cached.cpp
+++ OpenCL-matrix-mult-cached/OpenCL-matrix-mult-cached/OpenCL-matrix-mult-cached.cpp
@@ -2,8 +2,9 @@
 #include <fstream>
 #include <iostream>
 #include <algorithm>
-#include <time.h>
 #include <thread>
+#include <sstream>
+#include <chrono>
 
 // =================================================================
 // ---------------------- Secondary Functions ----------------------
@@ -17,12 +18,24 @@ void seqMultiplyMatrices(int* a,
 	const int M,
 	const int N,
 	const int K); // Sequentially performs the operation c[M,N] = a[M,K] * b[K,N].
-void parMultiplyMatrices(int* a,
+void parMultiplyMatricesNaive(int* a,
 	int* b,
 	int* c,
 	const int M,
 	const int N,
-	const int K); // Parallelly performs the operation c[M,N] = a[M,K] * b[K,N].
+	const int K); // Parallelly performs the operation c[M,N] = a[M,K] * b[K,N] (naive).
+void parMultiplyMatricesSubmatrix(int* a,
+	int* b,
+	int* c,
+	const int M,
+	const int N,
+	const int K); // Parallelly performs the operation c[M,N] = a[M,K] * b[K,N] (submatrix).
+void parMultiplyMatricesSubmatrixWpt(int* a,
+	int* b,
+	int* c,
+	const int M,
+	const int N,
+	const int K); // Parallelly performs the operation c[M,N] = a[M,K] * b[K,N] (submatrix + WRT).
 void parMultiplyMatrices_CPU(int* a,
 	int* b,
 	int* c,
@@ -38,36 +51,31 @@ bool checkEquality(int* c1,
 // ------------------------ Global Variables ------------------------
 // =================================================================
 
-cl::Program program;                // The program that will run on the device.    
 cl::Context context;                // The context which holds the device.    
 cl::Device device;                  // The device where the kernel will run.
-const size_t WG_SIZE[2] = { 16, 16 }; // The size of work-groups.
+
+cl::Program programNaive;                // The programs that will run on the device (naive).    
+cl::Program programSubMatrix;			// The programs that will run on the device (submatrix).    
+cl::Program programSubMatrixWpt;		// The programs that will run on the device (submatrix + WPT).    
+
+const size_t WPT = 8;				// The work per thread 
+const size_t SUB_SIZE = 16;		   // The default value of work group.
 
 // =================================================================
 // ------------------------- Main Function -------------------------
 // =================================================================
 
-int main() {
+int main() 
+{
+	// Create auxiliary variables.
+	const int EXECUTIONS = 10;
 
-	/**
-	 * Create auxiliary variables.
-	 * */
+	// Prepare input constants related to the dimensions of the matrices.
+	const int M = 1 << 10;
+	const int N = 1 << 10;
+	const int K = 1 << 10;
 
-	clock_t start, end;
-	const int EXECUTIONS = 40;
-
-	/**
-	 * Prepare input constants related to the dimensions of the matrices.
-	 * */
-
-	const int M = 1 << 9;
-	const int N = 1 << 9;
-	const int K = 1 << 9;
-
-	/**
-	 * Prepare input matrices A and B.
-	 * */
-
+	// Prepare input matrices A and B.
 	const size_t ROWS_A = M;
 	const size_t COLS_A = K;
 	std::vector<int> a(ROWS_A * COLS_A, 3);
@@ -76,70 +84,84 @@ int main() {
 	const size_t COLS_B = N;
 	std::vector<int> b(ROWS_B * COLS_B, 5);
 
-	/**
-	 * Prepare sequential and parallel output matrices.
-	 * */
-
+	// Prepare sequential and parallel output matrices.
 	const size_t ROWS_C = M;
 	const size_t COLS_C = N;
 	std::vector<int> cs(ROWS_C * COLS_C);
 	std::vector<int> cp(ROWS_C * COLS_C);
+	std::vector<int> cps(ROWS_C * COLS_C);
+	std::vector<int> cpsw(ROWS_C * COLS_C);
 	std::vector<int> cp_cpu(ROWS_C * COLS_C);
 
-	/**
-	 * Sequentially multiply matrices.
-	 * */
-
-	start = clock();
-	for (int i = 0; i < EXECUTIONS; i++) {
-		seqMultiplyMatrices(a.data(), b.data(), cs.data(), M, N, K);
+	// Sequentially multiply matrices.
+	double seqCpuMs = 0.0f;
+	{
+		auto t_start = std::chrono::high_resolution_clock::now();
+		for (int i = 0; i < EXECUTIONS; i++) {
+			seqMultiplyMatrices(a.data(), b.data(), cs.data(), M, N, K);
+		}
+		auto t_end = std::chrono::high_resolution_clock::now();
+		seqCpuMs = std::chrono::duration<double, std::milli>(t_end - t_start).count() / EXECUTIONS;
 	}
-	end = clock();
-	double seqTime = ((double) 10e3 * (end - start)) / CLOCKS_PER_SEC / EXECUTIONS;
 
-	/**
-	 * Parallel multiply matrices on CPU.
-	 * */
-
-	start = clock();
-	for (int i = 0; i < EXECUTIONS; i++) {
-		parMultiplyMatrices_CPU(a.data(), b.data(), cp_cpu.data(), M, N, K);
+	// Parallel multiply matrices on CPU.
+	double parCpuMs = 0.0f;
+	{
+		auto t_start = std::chrono::high_resolution_clock::now();
+		for (int i = 0; i < EXECUTIONS; i++) {
+			parMultiplyMatrices_CPU(a.data(), b.data(), cp_cpu.data(), M, N, K);
+		}
+		auto t_end = std::chrono::high_resolution_clock::now();
+		parCpuMs = std::chrono::duration<double, std::milli>(t_end - t_start).count() / EXECUTIONS;
 	}
-	end = clock();
-	double parCpuTime = ((double) 10e3 * (end - start)) / CLOCKS_PER_SEC / EXECUTIONS;
 
-	/**
-	 * Initialize OpenCL device.
-	 * */
-
+	// Initialize OpenCL device.
 	initializeDevice();
 
-	/**
-	 * Parallelly multiply matrices.
-	 * */
-
-	start = clock();
-	for (int i = 0; i < EXECUTIONS; i++) {
-		parMultiplyMatrices(a.data(), b.data(), cp.data(), M, N, K);
+	// Parallelly multiply matrices
+	double parGpuNaiveMs = 0.0f;
+	{
+		auto t_start = std::chrono::high_resolution_clock::now();
+		for (int i = 0; i < EXECUTIONS; i++) {
+			parMultiplyMatricesNaive(a.data(), b.data(), cp.data(), M, N, K);
+		}
+		auto t_end = std::chrono::high_resolution_clock::now();
+		parGpuNaiveMs = std::chrono::duration<double, std::milli>(t_end - t_start).count() / EXECUTIONS;
 	}
-	end = clock();
-	double parTime = ((double) 10e3 * (end - start)) / CLOCKS_PER_SEC / EXECUTIONS;
 
-	/**
-	 * Check if outputs are equal.
-	 * */
+	double parGpuSubMatrMs = 0.0f;
+	{
+		auto t_start = std::chrono::high_resolution_clock::now();
+		for (int i = 0; i < EXECUTIONS; i++) {
+			parMultiplyMatricesSubmatrix(a.data(), b.data(), cps.data(), M, N, K);
+		}
+		auto t_end = std::chrono::high_resolution_clock::now();
+		parGpuSubMatrMs = std::chrono::duration<double, std::milli>(t_end - t_start).count() / EXECUTIONS;
+	}
 
+	double parGpuSubMatrWptMs = 0.0f;
+	{
+		auto t_start = std::chrono::high_resolution_clock::now();
+		for (int i = 0; i < EXECUTIONS; i++) {
+			parMultiplyMatricesSubmatrixWpt(a.data(), b.data(), cpsw.data(), M, N, K);
+		}
+		auto t_end = std::chrono::high_resolution_clock::now();
+		parGpuSubMatrWptMs = std::chrono::duration<double, std::milli>(t_end - t_start).count() / EXECUTIONS;
+	}
+
+	// Check if outputs are equal.
 	bool equal1 = checkEquality(cs.data(), cp.data(), ROWS_C, COLS_C);
-	bool equal2 = checkEquality(cs.data(), cp_cpu.data(), ROWS_C, COLS_C);
+	bool equal2 = checkEquality(cs.data(), cps.data(), ROWS_C, COLS_C);
+	bool equal3 = checkEquality(cs.data(), cpsw.data(), ROWS_C, COLS_C);
+	bool equal4 = checkEquality(cs.data(), cp_cpu.data(), ROWS_C, COLS_C);
 
-	/**
-	 * Print results.
-	 * */
-
-	std::cout << "Status: " << (equal1 && equal2 ? "SUCCESS!" : "FAILED!") << std::endl;
+	// Print results.
+	std::cout << "Status: " << (equal1 && equal2 && equal3 && equal4 ? "SUCCESS!" : "FAILED!") << std::endl;
 	std::cout << "Results: \n\tA[0] = " << a[0] << "\n\tB[0] = " << b[0] << "\n\tC[0] = " << cp[0] << std::endl;
-	std::cout << "Mean execution time: \n\tSequential: " << seqTime << " ms;\n\tParallel: " << parTime << " ms;\n\tParallel_CPU: " << parCpuTime << " ms." << std::endl;
-	std::cout << "Performance gain: " << (100 * (seqTime - parTime) / parTime) << "\%\n";
+	std::cout << "Mean execution time: \n\tSequential CPU: " << seqCpuMs << " ms;\n\tParallel (naive): " << parGpuNaiveMs 
+		<< " ms;\n\tParallelSubmatrix: " << parGpuSubMatrMs
+		<< " ms;\n\tParallelSubmatrixWpt: " << parGpuSubMatrWptMs
+		<< " ms;\n\tParallel_CPU: " << parCpuMs << " ms." << std::endl;
 	return 0;
 }
 
@@ -147,17 +169,11 @@ int main() {
 // ---------------------- Secondary Functions ----------------------
 // =================================================================
 
-/**
- * Return the first device found in this OpenCL platform.
- * */
-
-cl::Device getDefaultDevice() {
-
-	/**
-	 * Search for all the OpenCL platforms available and check
-	 * if there are any.
-	 * */
-
+// Return the first device found in this OpenCL platform.
+cl::Device getDefaultDevice() 
+{
+	 // Search for all the OpenCL platforms available and check
+	 // if there are any.
 	std::vector<cl::Platform> platforms;
 	cl::Platform::get(&platforms);
 
@@ -166,66 +182,73 @@ cl::Device getDefaultDevice() {
 		exit(1);
 	}
 
-	/**
-	 * Search for all the devices on the first platform and check if
-	 * there are any available.
-	 * */
 
+	 // Search for all the devices on the first platform and check if
+	 // there are any available.
 	auto platform = platforms.front();
 	std::vector<cl::Device> devices;
 	platform.getDevices(CL_DEVICE_TYPE_ALL, &devices);
 
-	if (devices.empty()) {
+	if (devices.empty()) 
+	{
 		std::cerr << "No devices found!" << std::endl;
 		exit(1);
 	}
 
-	/**
-	 * Return the first device found.
-	 * */
-
+	// Return the first device found.
 	return devices.front();
 }
 
-/**
- * Inicialize device and compile kernel code.
- * */
-
-void initializeDevice() {
-
-	/**
-	 * Select the first available device.
-	 * */
-
+// Inicialize device and compile kernel code.
+void initializeDevice() 
+{
+	// Select the first available device.
 	device = getDefaultDevice();
 
-	/**
-	 * Read OpenCL kernel file as a string.
-	 * */
-
+	// Read OpenCL kernel file as a string.
 	std::ifstream kernel_file("cached_matrix_multiplication.cl");
 	std::string src(std::istreambuf_iterator<char>(kernel_file), (std::istreambuf_iterator<char>()));
 
-	/**
-	 * Compile kernel program which will run on the device.
-	 * */
-
+	// Compile kernel program which will run on the device.
 	cl::Program::Sources sources(1, std::make_pair(src.c_str(), src.length() + 1));
 	context = cl::Context(device);
-	program = cl::Program(context, sources);
 
-	auto err = program.build();
-	if (err != CL_BUILD_SUCCESS) {
-		std::cerr << "Error!\nBuild Status: " << program.getBuildInfo<CL_PROGRAM_BUILD_STATUS>(device)
-			<< "\nBuild Log:\t " << program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(device) << std::endl;
-		exit(1);
+	{
+		programNaive = cl::Program(context, sources);
+		auto err = programNaive.build();
+		if (err != CL_BUILD_SUCCESS) {
+			std::cerr << "Error!\nBuild Status: " << programNaive.getBuildInfo<CL_PROGRAM_BUILD_STATUS>(device)
+				<< "\nBuild Log:\t " << programNaive.getBuildInfo<CL_PROGRAM_BUILD_LOG>(device) << std::endl;
+			exit(1);
+		}
+	}
+
+	{
+		programSubMatrix = cl::Program(context, sources);
+		std::stringstream ss;
+		ss <<" -D SUB_SIZE=" << SUB_SIZE; // defines for kernel function
+		auto err = programSubMatrix.build(ss.str().c_str());
+		if (err != CL_BUILD_SUCCESS) {
+			std::cerr << "Error!\nBuild Status: " << programSubMatrix.getBuildInfo<CL_PROGRAM_BUILD_STATUS>(device)
+				<< "\nBuild Log:\t " << programSubMatrix.getBuildInfo<CL_PROGRAM_BUILD_LOG>(device) << std::endl;
+			exit(1);
+		}
+	}
+
+	{
+		programSubMatrixWpt = cl::Program(context, sources);
+		std::stringstream ss;
+		ss << "-D WPT=" << WPT << " -D SUB_SIZE=" << SUB_SIZE; // defines for kernel function
+		auto err = programSubMatrixWpt.build(ss.str().c_str());
+		if (err != CL_BUILD_SUCCESS) {
+			std::cerr << "Error!\nBuild Status: " << programSubMatrixWpt.getBuildInfo<CL_PROGRAM_BUILD_STATUS>(device)
+				<< "\nBuild Log:\t " << programSubMatrixWpt.getBuildInfo<CL_PROGRAM_BUILD_LOG>(device) << std::endl;
+			exit(1);
+		}
 	}
 }
 
-/**
- * Sequentially performs the operation c[M,N] = a[M,K] * b[K,N].
- * */
-
+// Sequentially performs the operation c[M,N] = a[M,K] * b[K,N].
 void seqMultiplyMatrices(int* a, int* b, int* c,
 	const int M,
 	const int N,
@@ -241,10 +264,7 @@ void seqMultiplyMatrices(int* a, int* b, int* c,
 	}
 }
 
-/**
- * Parallelly performs the operation (on CPU) c[M,N] = a[M,K] * b[K,N].
- * */
-
+// Parallelly performs the operation (on CPU) c[M,N] = a[M,K] * b[K,N].
 void parMultiplyMatrices_CPU(int* a, int* b, int* c,
 	const int M,
 	const int N,
@@ -291,28 +311,19 @@ void parMultiplyMatrices_CPU(int* a, int* b, int* c,
 	workers.clear();
 }
 
-/**
- * Parallelly performs the operation c[M,N] = a[M,K] * b[K,N].
- * */
-
-void parMultiplyMatrices(int* a, int* b, int* c,
+// Parallelly performs the operation c[M,N] = a[M,K] * b[K,N].
+void parMultiplyMatricesNaive(int* a, int* b, int* c,
 	const int M,
 	const int N,
-	const int K) {
-
-	/**
-	 * Create buffers and allocate memory on the device.
-	 * */
-
+	const int K) 
+{
+	// Create buffers and allocate memory on the device.
 	cl::Buffer aBuf(context, CL_MEM_READ_ONLY | CL_MEM_HOST_NO_ACCESS | CL_MEM_COPY_HOST_PTR, M * K * sizeof(int), a);
 	cl::Buffer bBuf(context, CL_MEM_READ_ONLY | CL_MEM_HOST_NO_ACCESS | CL_MEM_COPY_HOST_PTR, K * N * sizeof(int), b);
 	cl::Buffer cBuf(context, CL_MEM_READ_WRITE | CL_MEM_HOST_READ_ONLY, M * N * sizeof(int));
 
-	/**
-	 * Set kernel arguments.
-	 * */
-
-	cl::Kernel kernel(program, "multiplyMatricesWithCache");
+	// Set kernel arguments.
+	cl::Kernel kernel(programNaive, "multiplyMatrices");
 	kernel.setArg(0, aBuf);
 	kernel.setArg(1, bBuf);
 	kernel.setArg(2, cBuf);
@@ -320,22 +331,72 @@ void parMultiplyMatrices(int* a, int* b, int* c,
 	kernel.setArg(4, sizeof(unsigned int), &N);
 	kernel.setArg(5, sizeof(unsigned int), &K);
 
-	/**
-	 * Execute the kernel function and collect its result.
-	 * */
-
+	// Execute the kernel function and collect its result.
 	cl::CommandQueue queue(context, device);
-	queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(N, M), cl::NDRange(WG_SIZE[0], WG_SIZE[1]));
+	queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(N, M));
 	queue.enqueueReadBuffer(cBuf, CL_TRUE, 0, M * N * sizeof(int), c);
+	queue.finish();
 }
 
-/**
- * Check if the matrices C1 and C2 are equal.
- * */
+// Parallelly performs the operation c[M,N] = a[M,K] * b[K,N].
+void parMultiplyMatricesSubmatrix(int* a, int* b, int* c,
+	const int M,
+	const int N,
+	const int K) 
+{
+	// Create buffers and allocate memory on the device.
+	cl::Buffer aBuf(context, CL_MEM_READ_ONLY | CL_MEM_HOST_NO_ACCESS | CL_MEM_COPY_HOST_PTR, M * K * sizeof(int), a);
+	cl::Buffer bBuf(context, CL_MEM_READ_ONLY | CL_MEM_HOST_NO_ACCESS | CL_MEM_COPY_HOST_PTR, K * N * sizeof(int), b);
+	cl::Buffer cBuf(context, CL_MEM_READ_WRITE | CL_MEM_HOST_READ_ONLY, M * N * sizeof(int));
 
+	// Set kernel arguments.
+	cl::Kernel kernel(programSubMatrix, "multiplyMatrices");
+	kernel.setArg(0, aBuf);
+	kernel.setArg(1, bBuf);
+	kernel.setArg(2, cBuf);
+	kernel.setArg(3, sizeof(unsigned int), &M);
+	kernel.setArg(4, sizeof(unsigned int), &N);
+	kernel.setArg(5, sizeof(unsigned int), &K);
+
+	// Execute the kernel function and collect its result.
+	cl::CommandQueue queue(context, device);
+	queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(N, M), cl::NDRange(SUB_SIZE, SUB_SIZE));
+	queue.enqueueReadBuffer(cBuf, CL_TRUE, 0, M * N * sizeof(int), c);
+	queue.finish();
+}
+
+// Parallelly performs the operation c[M,N] = a[M,K] * b[K,N].
+void parMultiplyMatricesSubmatrixWpt(int* a, int* b, int* c,
+	const int M,
+	const int N,
+	const int K) 
+{
+	// Create buffers and allocate memory on the device.
+	cl::Buffer aBuf(context, CL_MEM_READ_ONLY | CL_MEM_HOST_NO_ACCESS | CL_MEM_COPY_HOST_PTR, M * K * sizeof(int), a);
+	cl::Buffer bBuf(context, CL_MEM_READ_ONLY | CL_MEM_HOST_NO_ACCESS | CL_MEM_COPY_HOST_PTR, K * N * sizeof(int), b);
+	cl::Buffer cBuf(context, CL_MEM_READ_WRITE | CL_MEM_HOST_READ_ONLY, M * N * sizeof(int));
+
+	// Set kernel arguments.
+	cl::Kernel kernel(programSubMatrixWpt, "multiplyMatrices");
+	kernel.setArg(0, aBuf);
+	kernel.setArg(1, bBuf);
+	kernel.setArg(2, cBuf);
+	kernel.setArg(3, sizeof(unsigned int), &M);
+	kernel.setArg(4, sizeof(unsigned int), &N);
+	kernel.setArg(5, sizeof(unsigned int), &K);
+
+	// Execute the kernel function and collect its result.
+	cl::CommandQueue queue(context, device);
+	queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(N, M / WPT), cl::NDRange(SUB_SIZE, SUB_SIZE / WPT));
+	queue.enqueueReadBuffer(cBuf, CL_TRUE, 0, M * N * sizeof(int), c);
+	queue.finish();
+}
+
+// Check if the matrices C1 and C2 are equal.
 bool checkEquality(int* c1, int* c2,
 	const int M,
-	const int N) {
+	const int N)
+{
 	for (int i = 0; i < M*N; i++) {
 		if (c1[i] != c2[i]) {
 			return false;
--- OpenCL-matrix-mult-cached/OpenCL-matrix-mult-cached/cached_matrix_multiplication.cl
+++ OpenCL-matrix-mult-cached/OpenCL-matrix-mult-cached/cached_matrix_multiplication.cl
@@ -1,85 +1,146 @@
+#if !defined(SUB_SIZE) && !defined(WPT)
 /**
- * This kernel function efficiently multiplies two matrices a[M,K] and b[K,N] 
- * by caching submatrices from those input matrices in the device local memory.
+ * This kernel function efficiently multiplies two matrices a[M,K] and b[K,N] by "naive" way
  */
+__kernel void multiplyMatrices(__global int* a,
+	__global int* b,
+	__global int* c,
+	const int M,
+	const int N,
+	const int K)
+{
+	//Get work - item identifiers.
+	int colIndex = get_global_id(0);
+	int rowIndex = get_global_id(1);
 
-__kernel void multiplyMatricesWithCache(__global int* a,
+	//Compute element c[rowIndex, colIndex].
+	int sum = 0;
+	for (int k = 0; k < K; k++) {
+		sum += a[rowIndex*K + k] * b[k*N + colIndex];
+	}
+	// save result
+	c[(rowIndex * N) + colIndex] = sum;
+}
+
+#endif
+
+#if defined(SUB_SIZE) && !defined(WPT)
+/**
+ * This kernel function efficiently multiplies two matrices a[M,K] and b[K,N] by multiply submatrix
+ */
+__kernel void multiplyMatrices(__global int* a,
+	__global int* b,
+	__global int* c,
+	const int M,
+	const int N,
+	const int K)
+{
+	// Get work-item identifiers.
+	int colIndex = get_local_id(0);
+	int rowIndex = get_local_id(1);
+	int globalColIndex = get_global_id(0);
+	int globalRowIndex = get_global_id(1);
+	int index = (globalRowIndex * N) + globalColIndex;
+
+	// Create submatrices that will cache the matrices A and B in local memory.
+	__local int aSub[SUB_SIZE][SUB_SIZE];
+	__local int bSub[SUB_SIZE][SUB_SIZE];
+
+	// Initialize accumulator register.
+	int sum = 0;
+
+	// Loop over all submatrices.
+	const int nSub = K / SUB_SIZE;
+	for (int s = 0; s < nSub; s++)
+	{
+		// Load submatrices into local memory.
+		const int sCol = SUB_SIZE * s + colIndex;
+		const int sRow = SUB_SIZE * s + rowIndex;
+		aSub[rowIndex][colIndex] = a[globalRowIndex * K + sCol];
+		bSub[rowIndex][colIndex] = b[sRow * N + globalColIndex];
+
+		// Synchronize all work-items in this work-group.
+		barrier(CLK_LOCAL_MEM_FENCE);
+
+		// Perform the computation for a single submatrix.
+		for (int k = 0; k < SUB_SIZE; k++) {
+			sum += aSub[rowIndex][k] * bSub[k][colIndex];
+		}
+
+		// Synchronize all work-items in this work-group.
+		barrier(CLK_LOCAL_MEM_FENCE);
+	}
+
+	// Store the final result in the matrix C.
+	c[index] = sum;
+}
+
+#endif
+
+#if defined(SUB_SIZE) && defined(WPT)
+/**
+ * This kernel function efficiently multiplies two matrices a[M,K] and b[K,N]
+ * by caching submatrices from those input matrices in the device local memory
+ * and use "work per thread" method.
+ */
+__kernel void multiplyMatrices(__global int* a,
                                     __global int* b,
                                     __global int* c,
                                     const int M,
                                     const int N, 
-                                    const int K){
+                                    const int K)
+{
+	const int RTS = SUB_SIZE / WPT;
+	// Get work-item identifiers.
+	const int row = get_local_id(0); // Local row ID (max: SUB_SIZE)
+	const int col = get_local_id(1); // Local col ID (max: SUB_SIZE/WPT == RTS)
+	const int globalRow = SUB_SIZE * get_group_id(0) + row; // Row ID of C (0..M)
+	const int globalCol = SUB_SIZE * get_group_id(1) + col; // Col ID of C (0..N)
 
-    /**
-     * Declare the size of each submatrix (it must be 
-     * the same work-group size declared in the host code).
-     */
-
-    const int SUB_SIZE = 16;
-    
-    /**
-     * Get work-item identifiers.
-     */
-    
-    int colIndex = get_local_id(0);
-    int rowIndex = get_local_id(1);
-    int globalColIndex = get_global_id(0);
-    int globalRowIndex = get_global_id(1);
-    int index = (globalRowIndex * N) + globalColIndex;
-
-    /**
-     * Create submatrices that will cache the matrices A and B in local memory.
-     */
-
+	// Create submatrices that will cache the matrices A and B in local memory.
     __local int aSub[SUB_SIZE][SUB_SIZE];
     __local int bSub[SUB_SIZE][SUB_SIZE];
 
-    /**
-     * Initialize accumulator register.
-     */
+	// Initialize accumulator 
+	float acc[WPT];
+	for (int w = 0; w < WPT; w++) 
+	{
+		acc[w] = 0.0f;
+	}
 
-    int sum = 0;
-
-    /**
-     * Loop over all submatrices.
-     */
-
+	// Loop over all submatrices.
     const int nSub = K / SUB_SIZE;
-    for(int s = 0; s < nSub; s++){
+    for(int s = 0; s < nSub; s++)
+	{
+		// Load one tile of A and B into local memory
+		for (int w = 0; w < WPT; w++) 
+		{
+			const int tiledRow = SUB_SIZE * s + row;
+			const int tiledCol = SUB_SIZE * s + col;
+			aSub[col + w * RTS][row] = a[(tiledCol + w * RTS)*M + globalRow];
+			bSub[col + w * RTS][row] = b[(globalCol + w * RTS)*K + tiledRow];
+		}
 
-        /**
-         * Load submatrices into local memory.
-         */
-
-        const int sCol = SUB_SIZE * s + colIndex;
-        const int sRow = SUB_SIZE * s + rowIndex;
-        aSub[rowIndex][colIndex] = a[globalRowIndex * K + sCol];
-        bSub[rowIndex][colIndex] = b[sRow * N + globalColIndex];
-
-        /**
-         * Synchronize all work-items in this work-group.
-         */
-
+		// Synchronize all work-items in this work-group.
         barrier(CLK_LOCAL_MEM_FENCE);
 
-        /**
-         * Perform the computation for a single submatrix.
-         */
-        
-        for(int k = 0; k < SUB_SIZE; k++){
-            sum += aSub[rowIndex][k] * bSub[k][colIndex];
-        }
+		// Perform the computation for a single tile
+		for (int k = 0; k < SUB_SIZE; k++)
+		{
+			for (int w = 0; w < WPT; w++) 
+			{
+				acc[w] += aSub[k][row] * bSub[col + w * RTS][k];
+			}
+		}
 
-        /**
-         * Synchronize all work-items in this work-group.
-         */
-
+		// Synchronize all work-items in this work-group.
         barrier(CLK_LOCAL_MEM_FENCE);
     }
 
-    /**
-     * Store the final result in the matrix C.
-     */
-
-    c[index] = sum;
+	// Store the final results in C
+	for (int w = 0; w < WPT; w++) 
+	{
+		c[(globalCol + w * RTS)*M + globalRow] = acc[w];
+	}
 }
+#endif
